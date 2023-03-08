dff6c0aea02d49c99f01d93d6a4f6b9ac54b1111
Masa Orel <orel.masha89@gmail.com>
2023-03-08 15:26:59 +0100

    small task

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import { useState } from "react";
 import logo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
-      <div className="logo">
-        <img src={logo} alt="" />
-      </div>
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+      <header>
+        <nav>
+          <img src={logo} alt="" />
+        </nav>
+      </header>
+
+      <h1>Reasons why i'm excited to learn react</h1>
+      <ol>
+        <li>
+          It's a popular library, so I'll be able to fit in with the cool kids!
+        </li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
+
+      <footer>
+        <small> "© 2023 Orel development. All rights reserved."</small>
+      </footer>
     </div>
   );
 }
